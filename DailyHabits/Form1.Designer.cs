e0d49a7a69187355ba8b1ae6f4de09d3787548b9
gersonnascimento <gerson.nascimento@locaweb.com.br>
2017-06-07 22:20:42 -0300

    atualizado tamanho dos paineis

--- DailyHabits/Form1.Designer.cs
+++ DailyHabits/Form1.Designer.cs
@@ -29,13 +29,13 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
             this.hb1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.hb2 = new System.Windows.Forms.Label();
-            this.hb3 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
             this.hb4 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.hb3 = new System.Windows.Forms.Label();
             this.panel5 = new System.Windows.Forms.Panel();
             this.hb5 = new System.Windows.Forms.Label();
             this.pictureBox5 = new System.Windows.Forms.PictureBox();
@@ -62,40 +62,9 @@
             this.panel1.Controls.Add(this.hb1);
             this.panel1.Location = new System.Drawing.Point(12, 103);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(468, 100);
+            this.panel1.Size = new System.Drawing.Size(649, 100);
             this.panel1.TabIndex = 0;
             this.panel1.Visible = false;
-            // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.Color.Red;
-            this.panel2.Controls.Add(this.hb2);
-            this.panel2.Location = new System.Drawing.Point(12, 223);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(468, 100);
-            this.panel2.TabIndex = 2;
-            this.panel2.Visible = false;
-            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
-            // 
-            // panel3
-            // 
-            this.panel3.BackColor = System.Drawing.Color.Red;
-            this.panel3.Controls.Add(this.hb4);
-            this.panel3.Location = new System.Drawing.Point(12, 463);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(468, 100);
-            this.panel3.TabIndex = 6;
-            this.panel3.Visible = false;
-            // 
-            // panel4
-            // 
-            this.panel4.BackColor = System.Drawing.Color.Red;
-            this.panel4.Controls.Add(this.hb3);
-            this.panel4.Location = new System.Drawing.Point(12, 343);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(468, 100);
-            this.panel4.TabIndex = 4;
-            this.panel4.Visible = false;
             // 
             // hb1
             // 
@@ -103,12 +72,23 @@
             this.hb1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.hb1.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.hb1.ForeColor = System.Drawing.Color.Black;
-            this.hb1.Location = new System.Drawing.Point(136, 15);
+            this.hb1.Location = new System.Drawing.Point(51, 17);
             this.hb1.Name = "hb1";
             this.hb1.Size = new System.Drawing.Size(166, 67);
             this.hb1.TabIndex = 0;
             this.hb1.Text = "label1";
             this.hb1.Click += new System.EventHandler(this.hb1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Red;
+            this.panel2.Controls.Add(this.hb2);
+            this.panel2.Location = new System.Drawing.Point(12, 223);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(649, 100);
+            this.panel2.TabIndex = 2;
+            this.panel2.Visible = false;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
             // hb2
             // 
@@ -116,25 +96,22 @@
             this.hb2.Cursor = System.Windows.Forms.Cursors.Hand;
             this.hb2.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.hb2.ForeColor = System.Drawing.Color.Black;
-            this.hb2.Location = new System.Drawing.Point(136, 18);
+            this.hb2.Location = new System.Drawing.Point(51, 14);
             this.hb2.Name = "hb2";
             this.hb2.Size = new System.Drawing.Size(166, 67);
             this.hb2.TabIndex = 1;
             this.hb2.Text = "label1";
             this.hb2.Click += new System.EventHandler(this.hb2_Click);
             // 
-            // hb3
+            // panel3
             // 
-            this.hb3.AutoSize = true;
-            this.hb3.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.hb3.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.hb3.ForeColor = System.Drawing.Color.Black;
-            this.hb3.Location = new System.Drawing.Point(136, 18);
-            this.hb3.Name = "hb3";
-            this.hb3.Size = new System.Drawing.Size(166, 67);
-            this.hb3.TabIndex = 2;
-            this.hb3.Text = "label1";
-            this.hb3.Click += new System.EventHandler(this.hb3_Click);
+            this.panel3.BackColor = System.Drawing.Color.Red;
+            this.panel3.Controls.Add(this.hb4);
+            this.panel3.Location = new System.Drawing.Point(12, 463);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(649, 100);
+            this.panel3.TabIndex = 6;
+            this.panel3.Visible = false;
             // 
             // hb4
             // 
@@ -142,12 +119,35 @@
             this.hb4.Cursor = System.Windows.Forms.Cursors.Hand;
             this.hb4.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.hb4.ForeColor = System.Drawing.Color.Black;
-            this.hb4.Location = new System.Drawing.Point(136, 16);
+            this.hb4.Location = new System.Drawing.Point(42, 12);
             this.hb4.Name = "hb4";
             this.hb4.Size = new System.Drawing.Size(166, 67);
             this.hb4.TabIndex = 3;
             this.hb4.Text = "label1";
             this.hb4.Click += new System.EventHandler(this.hb4_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Red;
+            this.panel4.Controls.Add(this.hb3);
+            this.panel4.Location = new System.Drawing.Point(12, 343);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(649, 100);
+            this.panel4.TabIndex = 4;
+            this.panel4.Visible = false;
+            // 
+            // hb3
+            // 
+            this.hb3.AutoSize = true;
+            this.hb3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.hb3.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hb3.ForeColor = System.Drawing.Color.Black;
+            this.hb3.Location = new System.Drawing.Point(42, 17);
+            this.hb3.Name = "hb3";
+            this.hb3.Size = new System.Drawing.Size(166, 67);
+            this.hb3.TabIndex = 2;
+            this.hb3.Text = "label1";
+            this.hb3.Click += new System.EventHandler(this.hb3_Click);
             // 
             // panel5
             // 
@@ -155,7 +155,7 @@
             this.panel5.Controls.Add(this.hb5);
             this.panel5.Location = new System.Drawing.Point(12, 584);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(468, 100);
+            this.panel5.Size = new System.Drawing.Size(649, 100);
             this.panel5.TabIndex = 8;
             this.panel5.Visible = false;
             // 
@@ -165,7 +165,7 @@
             this.hb5.Cursor = System.Windows.Forms.Cursors.Hand;
             this.hb5.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.hb5.ForeColor = System.Drawing.Color.Black;
-            this.hb5.Location = new System.Drawing.Point(136, 19);
+            this.hb5.Location = new System.Drawing.Point(42, 17);
             this.hb5.Name = "hb5";
             this.hb5.Size = new System.Drawing.Size(166, 67);
             this.hb5.TabIndex = 3;
@@ -175,7 +175,7 @@
             // pictureBox5
             // 
             this.pictureBox5.Image = global::DailyHabits.Properties.Resources.low;
-            this.pictureBox5.Location = new System.Drawing.Point(507, 584);
+            this.pictureBox5.Location = new System.Drawing.Point(700, 584);
             this.pictureBox5.Name = "pictureBox5";
             this.pictureBox5.Size = new System.Drawing.Size(100, 100);
             this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -186,7 +186,7 @@
             // pictureBox3
             // 
             this.pictureBox3.Image = global::DailyHabits.Properties.Resources.low;
-            this.pictureBox3.Location = new System.Drawing.Point(507, 463);
+            this.pictureBox3.Location = new System.Drawing.Point(700, 463);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(100, 100);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -197,7 +197,7 @@
             // pictureBox4
             // 
             this.pictureBox4.Image = global::DailyHabits.Properties.Resources.low;
-            this.pictureBox4.Location = new System.Drawing.Point(507, 343);
+            this.pictureBox4.Location = new System.Drawing.Point(700, 343);
             this.pictureBox4.Name = "pictureBox4";
             this.pictureBox4.Size = new System.Drawing.Size(100, 100);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -208,7 +208,7 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = global::DailyHabits.Properties.Resources.low;
-            this.pictureBox2.Location = new System.Drawing.Point(507, 223);
+            this.pictureBox2.Location = new System.Drawing.Point(700, 223);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(100, 100);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -219,7 +219,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::DailyHabits.Properties.Resources.low;
-            this.pictureBox1.Location = new System.Drawing.Point(507, 103);
+            this.pictureBox1.Location = new System.Drawing.Point(700, 103);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(100, 100);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -244,7 +244,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Highlight;
-            this.ClientSize = new System.Drawing.Size(631, 717);
+            this.ClientSize = new System.Drawing.Size(847, 717);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox5);
             this.Controls.Add(this.panel5);
